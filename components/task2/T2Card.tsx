82298da979cf474c49eede36bd7b00163c2b1df7
Santosh B <sbohara579@gmail.com>
2026-04-09 11:06:28 +0545

    made minor changes

--- components/task2/T2Card.tsx
+++ components/task2/T2Card.tsx
@@ -9,8 +9,8 @@ type Props = {
 const T2Card = ({ heading, subheading, number }: Props) => {
   return (
     <div className="group relative w-70 px-[35.5px] flex flex-col items-center justify-center text-[#C33241] h-[461px] rounded-[32px] bg-red-100">
-      <div className="absolute -top-22 opacity-0 group-hover:opacity-100 transition-opacity duration-500">
-        <Image src="/arrow.png" alt="Click me" width={120} height={120} />
+      <div className="absolute -top-22 opacity-0  group-hover:opacity-100 invisible group-hover:visible transition-opacity duration-500">
+        <Image src="/task2/arrow.png" alt="Click me" width={120} height={120} />
       </div>
       <div className="flex flex-col items-center">
         <div className="-rotate-[90deg] w-[218px] flex flex-1 flex-col gap-2">
