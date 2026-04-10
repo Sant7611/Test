adab4b2647dbf1e2f9c23baf9c7ecaccddf73f0b
Santosh B <sbohara579@gmail.com>
2026-04-10 21:19:10 +0545

    formate:images routing updated

--- components/task2/Card4.tsx
+++ components/task2/Card4.tsx
@@ -5,7 +5,12 @@ export default function Card4() {
   return (
     <CardLayout backgroundColor={"#5492A0"}>
       <div className="absolute z-[1]  top-[38px] left-[62px] z-10 ">
-        <Image alt="profile Image" src="/check.png" height={329} width={572}  />
+        <Image
+          alt="profile Image"
+          src="/task1/check.png"
+          height={329}
+          width={572}
+        />
       </div>
       <div className="absolute w-[241px] h-[48px] top-[36px] left-[80px]">
         <p className="text-white font-nohemi font-bold text-[20px] leading-[120%] tracking-normal">
